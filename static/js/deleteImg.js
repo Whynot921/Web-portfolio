ac0be1a7f6555dfeb88d0b75954710246dec084c
Whynot921 <ranel.021408@gmail.com>
2025-04-23 16:54:53 +0300

    Update deleteImg.js

--- static/js/deleteImg.js
+++ static/js/deleteImg.js
@@ -1,34 +1,8 @@
-const CurrUrl = window.location.href;
-console.log(CurrUrl);
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+  const images = document.querySelectorAll(".gallery_img");
 
-document.querySelectorAll(".gallery_img").forEach((image) => {
-  image.addEventListener("click", function () {
-    const imageId = this.dataset.imageId;
-    const data = { ToDelete: imageId };
-    const confirmation = confirm(
-      "Вы уверены, что хотите удалить это изображение?"
-    );
-    if (confirmation) {
-      this.remove();
-      fetch(CurrUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.json();
-        })
-        .then((responseData) => {
-          console.log("Success:", responseData);
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    }
-  });
+  if (images.length > 0) {
+    gallery.classList.add("show"); // Добавляет класс show, если есть изображения
+  }
 });
